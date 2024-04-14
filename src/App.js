7fae59e8211e35358ac5b2ae4bf77879cae38416
Krishnakant Kumar <47590877+kant146@users.noreply.github.com>
2024-04-15 00:59:59 +0530

    ssr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React,{useState} from'react';
 import './App.css';
 
 function App() {
+  const [count,setcount] = useState(0);
+
+  const increment = () => {
+    setcount(count + 1)
+  }
+
+  const decrement = () => {
+    setcount(count - 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin:"auto",display:"flex",flexDirection:"column",justifyContent:"center",alignItems: "center",width:"100vh",height:"100vh",backgroundColor:"#cc6888"}}>
+    <p>{count}</p>
+    <div style={{gap:"2rem",display:"flex"}}>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={increment}>Increment</button>
+    
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+
+const renderOrHydrate = rootElement.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render;
+
+renderOrHydrate(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
